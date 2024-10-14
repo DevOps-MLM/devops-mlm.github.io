c8a363c519f6fff649350349d910a97ee4926c31
Rizky Fadillah <fadillahrizki16@gmail.com>
2024-10-14 22:03:33 +0700

    feat: archive

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -9,10 +9,20 @@ export const HomePage = () => {
         setData([...data,note])
     }
 
+    function handleAction(id){
+      setData(data.filter(note=>{
+          if(note.id===id){
+              note.archived = !note.archived
+          }
+          return note
+      }))
+    }
+
   return (
     <div className="note-app__body">
         <FormNote onAdd={handleAdd} />
-        <NoteList title={'Catatan Aktif'} data={data.filter(note=>!note.archived)}/>
+        <NoteList title={'Catatan Aktif'} data={data.filter(note=>!note.archived)} onAction={handleAction}/>
+        <NoteList title={'Arsip'} data={data.filter(note=>note.archived)} onAction={handleAction}/>
       </div>
   )
 }
