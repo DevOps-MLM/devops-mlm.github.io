175d0aec87a329311a5daad2b08195550217af1d
MhmmdSyahputra <putra.cokers@gmail.com>
2024-10-15 10:20:09 +0700

    feat: create serach data

--- src/features/home/listNote.jsx
+++ src/features/home/listNote.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/prop-types */
 import EmptyNote from "./emptyNote";
 import NoteItem from "./itemNote";
 
-export default function NoteList({title,data, onDelete, onAction}){
+// eslint-disable-next-line react/prop-types
+export default function NoteList({title, data, onDelete, onAction}){
 
     return(
         <>
             <h2>{title}</h2>
             {
+                // eslint-disable-next-line react/jsx-key
                 data.length ? <div className="notes-list">{data.map(note=><NoteItem note={note} onDelete={onDelete} onAction={onAction}/>)}</div> : <EmptyNote />
             }
         </>
