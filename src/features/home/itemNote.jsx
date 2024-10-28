92866f69984d58ab2d1862e15fd99b8dd015b3ad
MhmmdSyahputra <putra.cokers@gmail.com>
2024-10-28 21:00:08 +0700

    style: desain button edit

--- src/features/home/itemNote.jsx
+++ src/features/home/itemNote.jsx
@@ -13,7 +13,7 @@ export default function NoteItem({note, onDelete, onAction, onEdit}){
             <div className="note-item__action">
                 <button className="note-item__delete-button" onClick={() => onDelete(note.id)}>Delete</button>
                 <button className="note-item__archive-button" onClick={() => onAction(note.id)}>{note.archived ? 'Pindahkan' : 'Arsipkan'}</button>
-                <button className="note-item__archive-button" onClick={() => onEdit(note.id)}>Edit</button>
+                <button className="note-item__edit-button" onClick={() => onEdit(note.id)}>Edit</button>
             </div>
         </div>
     )
